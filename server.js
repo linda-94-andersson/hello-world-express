57df8b19be556da30c99f19438f05d486b887927
Linda Andersson <linda.29.andersson@gmail.com>
2022-08-17 10:46:38 +0200

    Addedd env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(process.env.VAR_SOM_INTE_FINNS);
 });
 
 app.listen(PORT, () => {
